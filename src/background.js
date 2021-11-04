81cd1cc7ab79cbe58fd1a87e929c85eec5cc5226
Jones Durumsky <jonesdurumsky@gmail.com>
2021-11-04 17:21:51 +0100

    try to deploy

--- src/background.js
+++ src/background.js
@@ -2,13 +2,11 @@ class Background {
   constructor() {}
 
   draw() {
-    game.backgroundImages.forEach(function (img) {
-      //SCORE 0:
-
+    for (let img of game.backgroundImages) {
       //move back ground after player surpasses the 300 point
-      if (img.score <= game.player.score){
+      if (img.score <= game.player.score) {
         if (keyIsDown(RIGHT_ARROW) && game.player.x > 300) {
-          img.x -= img.speed;
+          img.x -= img.speed; 
           image(img.src, img.x, 0, WIDTH * 2, height);
           image(img.src, img.x + WIDTH * 2, 0, WIDTH * 2, height);
         } else {
@@ -26,15 +24,17 @@ class Background {
         if (img.x <= -(WIDTH * 2)) {
           img.x = 0;
         }
+        this.update(img.x);
+        console.log( game.player.x ,img.x )
       }
 
       //SCORE 1:
 
       //move back ground after player surpasses the 300 point
-      if (img.score <= game.player.score)  {
+      if (img.score <= game.player.score) {
         if (keyIsDown(RIGHT_ARROW) && game.player.x > 300) {
           img.x -= img.speed;
-          image(img.src, game.backgroundImages[2].src.x, 0, WIDTH * 2, height);
+          image(img.src, img.x, 0, WIDTH * 2, height);
           image(img.src, img.x + WIDTH * 2, 0, WIDTH * 2, height);
         } else {
           image(img.src, img.x, 0, WIDTH * 2, height);
@@ -101,6 +101,12 @@ class Background {
       //     img.x = 0;
       //   }
       // }
-    });
+    }
+  }
+
+  update(value) {
+    for (let img of game.backgroundImages) {
+      img.x = value;
+    }
   }
 }
--- src/game.js
+++ src/game.js
@@ -15,44 +15,44 @@ class Game {
     
     this.backgroundImages = [
       {
-        src: loadImage("/assets/background/background-1A.png"),
+        src: loadImage("assets/background/background-1A.png"),
         x: 0,
         speed: 0.5,
         score: 0,
       },
       {
-        src: loadImage("/assets/background/background-score1.png"),
+        src: loadImage("assets/background/background-score1.png"),
         x: 0,
         speed: 1.5,
         score: 1
       },
       {
-        src: loadImage("/assets/background/background-1B.png"),
+        src: loadImage("assets/background/background-1B.png"),
         x: 0,
         speed: 1.5,
         score: 0,
       },
       {
-        src: loadImage("/assets/background/background-1C.png"),
+        src: loadImage("assets/background/background-1C.png"),
         x: 0,
         speed: 2.5,
         score: 0,
       },
       
       {
-        src: loadImage("/assets/background/background-score2.png"),
+        src: loadImage("assets/background/background-score2.png"),
         x: 0,
         speed: 2.5,
         score: 2
       },
       {
-        src: loadImage("/assets/background/background-score3.png"),
+        src: loadImage("assets/background/background-score3.png"),
         x: 0,
         speed: 2.5,
         score: 3
       },
       {
-        src: loadImage("/assets/background/background-score4.png"),
+        src: loadImage("assets/background/background-score4.png"),
         x: 0,
         speed: 2.5,
         score: 4
@@ -60,39 +60,39 @@ class Game {
       
     ];
     this.playerWalkForward = [
-      { src: loadImage("/assets/player/standing/player-standing.png") },
-      { src: loadImage("/assets/player/walking forwards/player-walking-1.png") },
-      { src: loadImage("/assets/player/walking forwards/player-walking-2.png") },
-      { src: loadImage("/assets/player/walking forwards/player-walking-3.png") },
-      { src: loadImage("/assets/player/walking forwards/player-walking-4.png") },
-      { src: loadImage("/assets/player/walking forwards/player-walking-5.png") },
+      { src: loadImage("assets/player/standing/player-standing.png") },
+      { src: loadImage("assets/player/walking forwards/player-walking-1.png") },
+      { src: loadImage("assets/player/walking forwards/player-walking-2.png") },
+      { src: loadImage("assets/player/walking forwards/player-walking-3.png") },
+      { src: loadImage("assets/player/walking forwards/player-walking-4.png") },
+      { src: loadImage("assets/player/walking forwards/player-walking-5.png") },
     ];
     this.playerWalkBackward = [
-      { src: loadImage("/assets/player/walking backwards/player-back-1.png") },
-      { src: loadImage("/assets/player/walking backwards/player-back-2.png") },
-      { src: loadImage("/assets/player/walking backwards/player-back-3.png") },
-      { src: loadImage("/assets/player/walking backwards/player-back-4.png") },
-      { src: loadImage("/assets/player/walking backwards/player-back-5.png") },
+      { src: loadImage("assets/player/walking backwards/player-back-1.png") },
+      { src: loadImage("assets/player/walking backwards/player-back-2.png") },
+      { src: loadImage("assets/player/walking backwards/player-back-3.png") },
+      { src: loadImage("assets/player/walking backwards/player-back-4.png") },
+      { src: loadImage("assets/player/walking backwards/player-back-5.png") },
     ];
     this.playerPulledBack = [
-      { src: loadImage("/assets/player/pulled back/player-pulled-back.png") },
+      { src: loadImage("assets/player/pulled back/player-pulled-back.png") },
     ];
 
     this.playerJump = [
-      { src: loadImage("/assets/player/jump/player-jump-2.png") },
+      { src: loadImage("assets/player/jump/player-jump-2.png") },
     ];
 
     this.playerStroke = [
-      { src: loadImage("/assets/player/stroke/player-stroke-1.png") },
-      { src: loadImage("/assets/player/stroke/player-stroke-2.png") },
+      { src: loadImage("assets/player/stroke/player-stroke-1.png") },
+      { src: loadImage("assets/player/stroke/player-stroke-2.png") },
     ];
 
-    this.worm = loadImage('/assets/worms/worm.gif')
+    this.worm = loadImage('ssets/worms/worm.gif')
 
     this.collectableImages = [
-      { src: loadImage("/assets/collectables/collectable-1.png"), x: 1350, y: 450},
-      { src: loadImage("/assets/collectables/collectable-2.png"), x: 450, y: 1030},
-      { src: loadImage("/assets/collectables/collectable-3.png"), x: 700, y: 850},
+      { src: loadImage("assets/collectables/collectable-1.png"), x: 1350, y: 450},
+      { src: loadImage("assets/collectables/collectable-2.png"), x: 450, y: 1030},
+      { src: loadImage("assets/collectables/collectable-3.png"), x: 700, y: 850},
 
     ];
      
